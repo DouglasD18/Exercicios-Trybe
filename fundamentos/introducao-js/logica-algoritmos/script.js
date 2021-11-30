a7a74f09c56eb7c8669f18d2aa5c92d8e5f594a4
Douglas Aguiar <daguiaralcantara@gmail.com>
2021-11-30 18:50:31 -0300

    Resolução dos dois primeiros exercícios práticos.

---
+++ fundamentos/introducao-js/logica-algoritmos/script.js
@@ -0,0 +1,32 @@
+/* Exercício 1 */
+let n = 5;
+for (let i = 0; i < n; i++) {
+    let linhas = '';
+    for (let j =0; j < n; j++){
+        linhas += '*';
+    }
+    console.log(linhas);
+}
+console.log('\n');
+
+/* Exercício 2*/
+n = 5;
+for (let i = 1; i <= n; i++) {
+    let linhas = '';
+    for (let j =0; j < i; j++){
+        linhas += '*';
+    }
+    console.log(linhas);
+}
+console.log('\n');
+
+/* Exercício 3*/
+n = 5;
+for (let i = 1; i <= n; i++) {
+    let linhas = '';
+    for (let j =0; j < i; j++){
+        linhas += '*';
+    }
+    console.log(linhas);
+}
+console.log('\n');
