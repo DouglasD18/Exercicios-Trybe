43872e1b101924ba249cb01c1bb5effc698fcd8d
Douglas Aguiar <daguiaralcantara@gmail.com>
2021-12-04 18:55:38 -0300

    Resolução de parte do exercício do course sobre seletores no DOM.

---
+++ fundamentos/eventos-DOM/seletores/exercicios/exercicio1/script.js
@@ -0,0 +1,22 @@
+/*
+Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+*/
+document.querySelector('p').innerText = "Trabalhando na área e muito feliz com isso. Aprendendo e me sentindo mais realizado a cada dia.";
+
+let verde = document.querySelectorAll('.main-content');
+for (let i = 0; i < verde.length; i++) {
+       verde[i].style.backgroundColor = 'rgb(76,164,109)';
+}
+
+function mudaCor(classe) {
+       let fundoBranco = document.getElementsByClassName(classe);
+       fundoBranco[0].style.backgroundColor = 'white';
+}
+mudaCor('center-content');
+
+function corrije(tagName) {
+       document.getElementsByTagName(tagName)[0].innerText = 'Exercício 5.1 - JavaScript';
+}
+corrije('h1');
+
