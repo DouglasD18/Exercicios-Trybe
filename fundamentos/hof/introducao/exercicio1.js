f9945086e866e596d32339ba1d36833a2480dfb1
Douglas Aguiar <daguiaralcantara@gmail.com>
2022-01-18 23:13:40 -0300

    Primeiro exercício de hof's

---
+++ fundamentos/hof/introducao/exercicio1.js
@@ -0,0 +1,16 @@
+const capEmail = (fullName) => {
+  const emailName = fullName.toLowerCase().replace(' ', '_');
+  const emailAdress = `${emailName}@trybe.com`;
+  return { name: fullName, email: emailAdress};
+};
+
+const newEmployees = (func) => {
+  const employees = {
+    id1: func('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: func('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: func('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+console.log(newEmployees(capEmail));
