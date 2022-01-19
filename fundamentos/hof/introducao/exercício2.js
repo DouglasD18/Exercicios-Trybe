b5aaf45c8737fdde2b59e05d8b35ebf1c20ababf
Douglas Aguiar <daguiaralcantara@gmail.com>
2022-01-18 23:25:44 -0300

    Mais prática.

---
+++ fundamentos/hof/introducao/exercício2.js
@@ -0,0 +1,14 @@
+const check = (apostado, sorteado) => {
+  if (apostado === sorteado) {
+    console.log("Parabéns você ganhou");
+  } else {
+    console.log('Tente novamente');
+  }
+};
+
+const doSort = (func, apostado) => {
+  const sorteado = Math.round(Math.random() * 5) + 1;
+  func(apostado, sorteado);
+};
+
+doSort(check, 5);
