11f237849e6513e2b4a97f5d2a30d6d82656183d
Douglas Aguiar <daguiaralcantara@gmail.com>
2022-01-24 12:40:36 -0300

    Exercício com testes umitários em jest.

--- fundamentos/es6/introdution-jest/sum.test.js
+++ fundamentos/es6/introdution-jest/sum.test.js
@@ -1,5 +1,19 @@
-const sum = (a, b) => a + b;
+const sum = require('./sum.js');
 
-test('sums two values', () => {
-  expect(sum(2, 3)).toEqual(5);
+describe('Soma dois números', () => {
+  it('Soma 4 e 5', () => {
+    expect(sum(4, 5)).toEqual(9);
+  })
+
+  it('Soma 0 e 0', () => {
+    expect(sum(0, 0)).toEqual(0);
+  })
+
+  it('Testa se é lançado um erro quando recebe 4 e "5"', () => {
+    expect(() => sum(4, '5')).toThrow();
+  });
+
+  it('Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")', () => {
+    expect(() => sum(4, '5')).toThrowError(new Error('parameters must be numbers'));
+  });
 });
