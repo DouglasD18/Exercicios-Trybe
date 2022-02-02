22cd39f4338e7290063a86f3c2b3b64c1c8e6dcf
Douglas Aguiar <daguiaralcantara@gmail.com>
2022-02-02 10:55:17 -0300

    Retornando o valor das 10 principais cripto moedas em reais usando duas API's.

--- fundamentos/js-assincrono/fetch/api.js
+++ fundamentos/js-assincrono/fetch/api.js
@@ -1,37 +1,52 @@
 const ul = document.querySelector('ul');
-/* const fetch = require('node-fetch');
 
 const changeCurrency = async () => {
   const url = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.min.json'
   try {
     const response = await fetch(url);
-    const data = await response.json();
-    const realValue = data.brl.toFixed(2);
+    const objeto = await response.json();
+    const data = objeto.usd;
+    const realValue = data.brl;
     return realValue;
   } catch (erro) {
     return erro;
   }
 }
 
-console.log(changeCurrency()) */
-
-const fetchCoin = () =>{
+const fetchCoin = async () => {
   const url = `https://api.coincap.io/v2/assets`;
   ul.innerHTML = '';
 
-  fetch(url)
+  try {
+    const realValue = await changeCurrency();
+    const response = await fetch(url);
+    const objeto = await response.json()
+    const data = objeto.data;    
+    const coins = data.filter((element, index) => element && index <= 10)
+      coins.forEach((element) => {
+        const li = document.createElement('li');
+        const newValue = (parseFloat(element.priceUsd) * realValue).toFixed(2);
+        li.innerText = `${element.name} (${element.symbol}): ${newValue}.`;
+        ul.appendChild(li);
+        })
+  } catch (error) {
+    return error;
+  }
+
+  /* fetch(url)
     .then((response) => response.json())
     .then((objeto) => objeto.data)
     .then((data) => {
       const coins = data.filter((element, index) => element && index <= 10)
       coins.forEach((element) => {
         const li = document.createElement('li');
-        li.innerText = `${element.name} (${element.symbol}): ${parseFloat(element.priceUsd).toFixed(2)}.`;
+        const newValue = parseFloat(element.priceUsd).toFixed(2) * realValue;
+        li.innerText = `${element.name} (${element.symbol}): ${newValue}.`;
         ul.appendChild(li);
         })
     })
 
-    .catch((erro) => console.log(erro))
+    .catch((erro) => console.log(erro)) */
 }
 
 window.onload = () => fetchCoin();
