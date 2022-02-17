e906b024310f0d8a16f8b418d875b7026bc8dba9
Douglas Aguiar <daguiaralcantara@gmail.com>
2022-02-17 12:13:31 -0300

    Meu primeiro componente React de classe!

---
+++ frontend/introducao/hello-world/para-fixar/src/Component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Component extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>Douglas Aguiar</h1>
+        <p>Estudante de Desenvolvimento Web na Trybe</p>
+      </div>
+    )
+  }
+}
+
+export default Component;
